707d5d3b69d9117dc194a59acc570bbe3228a91f
iff <iff@ik.me>
2023-02-22 11:06:31 +0100

    add: setAlpha()

--- src/operations.cpp
+++ src/operations.cpp
@@ -2,6 +2,7 @@
 #include <src/headers/globals.h>
 
 #include <array>
+#include <cmath>
 
 using namespace std;
 
@@ -21,13 +22,13 @@ void NodeTriangles() {
 		throw std::invalid_argument("Invalid mesh data. Connectivity matrix node as function of triangle (mcnt) is out of bounds");
 
 	// helping array
-	for (int i = 0; i < numTriangles; ++i) {
+	for (uint i = 0; i < numTriangles; ++i) {
 		meshData[meshDataHelper + i - 1] = 0;
 	}
 
 	// creation of table
 	// loop over edges
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		int index1 = 0;
 		int index2 = 0;
 		int node1 = meshData[numTriangles + 4 * i - 4 - 1];
@@ -91,7 +92,7 @@ void NodeTriangles() {
 void NodeEdge() {
 	connectivityMatrixNodeEdge= array<vector<int>, 2>({vector<int>(numEdges), vector<int>(numEdges)});
 
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		connectivityMatrixNodeEdge[0][i] = meshData[numTriangles + 4 * i - 4 - 1];
 		connectivityMatrixNodeEdge[1][i] = meshData[numTriangles + 4 * i - 3 - 1];
 	}
@@ -101,7 +102,7 @@ void NodeEdge() {
 void TriangleEdge() {
 	connectivityMatrixTriangleEdge = array<vector<int>, 2>({vector<int>(numEdges), vector<int>(numEdges)});
 
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		connectivityMatrixTriangleEdge[0][i] = meshData[numTriangles + 4 * i - 2 - 1];
 		connectivityMatrixTriangleEdge[1][i] = meshData[numTriangles + 4 * i - 1 - 1];
 	}
@@ -110,7 +111,7 @@ void TriangleEdge() {
 void createVertexEdge() {
 	connectivityMatrixVertexEdge = array<vector<int>, 2>({vector<int>(numEdges), vector<int>(numEdges)});
 
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		// Search for two identical nodes of two neighbourhood, writtin in the mcvl
 		const int triangle1 = connectivityMatrixTriangleEdge[0][i];
 		const int triangle2 = connectivityMatrixTriangleEdge[1][i];
@@ -179,7 +180,7 @@ void createVertexEdge() {
 }
 
 void reorder() {
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		const int triangle1 = connectivityMatrixTriangleEdge[0][i];
 		const int triangle2 = connectivityMatrixTriangleEdge[1][i];
 		const int node1 = connectivityMatrixNodeEdge[0][i];
@@ -212,7 +213,7 @@ void reorder() {
 }
 
 void regenerateMeshData() {
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		meshData[numTriangleEdge + 4 * i - 4 - 1] = connectivityMatrixNodeEdge[0][i];
 		meshData[numTriangleEdge + 4 * i - 3 - 1] = connectivityMatrixNodeEdge[1][i];
 
@@ -241,7 +242,7 @@ void getBoundaryConditions() {
 
 	nodeBoundaryConditions = array<vector<int>, 2> {vector<int>(numNodes), vector<int>(numNodes)};
 
-	for (int i = 0; i < numEdges; ++i) {
+	for (uint i = 0; i < numEdges; ++i) {
 		int lcc = 0;
 		int ic = 0;
 		const int triangle1 = connectivityMatrixTriangleEdge[0][i];
@@ -334,7 +335,86 @@ void getBoundaryConditions() {
 	}
 }
 
-void setAlpha();
+void setAlpha() {
+	sector = vector<double>(numNodes);
+	alpha.fill(vector<double>(numEdges));
+
+	for (int i = 0; i < numEdges; ++i) {
+		int node1 = connectivityMatrixNodeEdge[0][i] - 1;
+		int node2 = connectivityMatrixNodeEdge[1][i] - 1;
+		int nodeVertex1 = connectivityMatrixVertexEdge[0][i] - 1;
+		int nodeVertex2 = connectivityMatrixVertexEdge[1][i] - 1;
+		int vertexX1 = x[node2] - x[node1];
+		int vertexY1 = y[node2] - y[node1];
+		int vertexV1 = sqrt(pow(vertexX1, 2) + pow(vertexY1, 2));
+
+		vertexX1 = vertexX1 / vertexV1;
+		vertexY1 = vertexY1 / vertexV1;
+
+		alpha[7 - 1][i] = vertexX1;
+		alpha[8 - 1][i] = vertexY1;
+
+		if (nodeVertex1 > 0) {
+			int vertexX2 = x[nodeVertex1] - x[node1];
+			int vertexY2 = y[nodeVertex1] - y[node1];
+			int vertexV2 = sqrt(pow(vertexX2, 2) + pow(vertexY2, 2));
+			int vertexX5 = x[nodeVertex1] - x[node2];
+			int vertexY5 = y[nodeVertex1] - y[node2];
+			int vertexV5 = sqrt(pow(vertexX5, 2) + pow(vertexY5, 2));
+
+			vertexX2 = vertexX2 / vertexV2;
+			vertexY2 = vertexY2 / vertexV2;
+			vertexX5 = vertexX5 / vertexV5;
+			vertexY5 = vertexY5 / vertexV5;
+
+			alpha[1 - 1][i] = acos(vertexX1 * vertexX2 + vertexY1 * vertexY2);
+			alpha[3 - 1][i] = acos(- vertexX1 * vertexX5 - vertexY1 * vertexY5);
+		}
+
+		if (nodeVertex2 > 0) {
+			int vertexX3 = x[nodeVertex2] - x[node1];
+			int vertexY3 = y[nodeVertex2] - y[node1];
+			int vertexV3 = sqrt(pow(vertexX3, 2) + pow(vertexY3, 2));
+			int vertexX4 = x[nodeVertex2] - x[node2];
+			int vertexY4 = y[nodeVertex2] - y[node2];
+			int vertexV4 = sqrt(pow(vertexX4, 2) + pow(vertexY4, 2));
+
+			vertexX3 = vertexX3 / vertexV3;
+			vertexY3 = vertexY3 / vertexV3;
+			vertexX4 = vertexX4 / vertexV4;
+			vertexY4 = vertexY4 / vertexV4;
+
+			alpha[2 - 1][i] = acos(vertexX1 * vertexX3 + vertexY1 * vertexY3);
+			alpha[4 - 1][i] = acos(- vertexX1 * vertexX4 - vertexY1 * vertexY4);
+
+		}
+		
+		// setting angular sector of each node
+		sector[node1] = sector[node1] + alpha[1 - 1][i] + alpha[2 - 1][i];
+		sector[node2] = sector[node2] + alpha[3 - 1][i] + alpha[4 - 1][i];
+
+		// setting the correct values of alpha 1, 2 ,3 and 4 in the boundaries
+		if (nodeVertex1 < 0) {
+			alpha[1 - 1][i] = alpha[2 - 1][i];
+			alpha[3 - 1][i] = alpha[4 - 1][i];
+		}
+		if (nodeVertex2 < 0) {
+			alpha[2 - 1][i] = alpha[1 - 1][i];
+			alpha[4 - 1][i] = alpha[3 - 1][i];
+		}
+
+		// beta and beta prime
+		alpha[5 - 1][i] = tan( 1.0/2 * alpha[1 - 1][i] ) + tan( 1.0/2 * alpha[2 - 1][i] );
+		alpha[6 - 1][i] = tan( 1.0/2 * alpha[3 - 1][i] ) + tan( 1.0/2 * alpha[4 - 1][i] );
+
+	}
+
+	// udate angular sectors
+	for (int i = 0; i < numNodes; ++i) {
+		sector[i] = 1.0/2 * sector[i];
+	}
+}
+
 void setMetric();
 void setduVar();
 void getFlux();
